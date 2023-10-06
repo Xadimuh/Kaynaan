4c1c4659532c5914542e56e2d00b6a7f51975497
AhmadouBk <cheikhahmadoubambakamara@esp.sn>
2023-10-07 00:05:12 +0200

    Ajout de nouvelles fonctionnalités

---
+++ Kaynan/UserData.swift
@@ -0,0 +1,13 @@
+//
+//  UserData.swift
+//  Kaynan
+//
+//  Created by Cheikh Ahmadou Bamba Kamara on 06/10/2023.
+//
+
+import SwiftUI
+import Combine
+
+class UserData: ObservableObject {
+    @Published var recommendedWaterIntake: Double = 0.0
+}
